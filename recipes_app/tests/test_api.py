f31e0393a19ab61bac60a5b1da0ada3fb35917a7
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-07-02 09:18:10 +0200

    added: RecipeAPITestCaseHappy, RecipeAPITestCaseUnhappy

--- recipes_app/tests/test_api.py
+++ recipes_app/tests/test_api.py
@@ -32,8 +32,50 @@ class RecipeAPITestCase(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED) # reponse for logged in user post
         # self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED) # set to Status Code 401 to pass the test when logged out
+        # self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_detail_recipe(self):
         url = reverse('recipe-detail', kwargs={'pk': self.recipe.id})
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class RecipeAPITestCaseHappy(APITestCase): 
+        
+    def setUp(self): 
+        self.user = User.objects.create_user(username='testuser', password='testpassword') #create testuser
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_list_post_recipe(self):
+        url = reverse('recipe-list')
+        data = {
+            'title':'Recipe1',
+            'description':'description',
+            'author':self.user.id
+        }
+        
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class RecipeAPITestCaseUnhappy(APITestCase): 
+        
+    def setUp(self): 
+        self.user = User.objects.create_user(username='testuser', password='testpassword') #create testuser
+        # self.token = Token.objects.create(user=self.user)
+        # self.client = APIClient()
+        # self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_list_post_recipe(self):
+        url = reverse('recipe-list')
+        data = {
+            'title':'Recipe1',
+            'description':'description',
+            'author':self.user.id
+        }
+        
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)        
+
+
